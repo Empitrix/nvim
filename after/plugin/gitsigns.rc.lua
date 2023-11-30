431f48cfdf4af4698e273e2ab54a4e18259c785a
Empitrix <empitrix@gmail.com>
2023-12-01 00:44:41 +0330

    Updated: gitsign delete icon

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -2,9 +2,17 @@ local status, gitsigns = pcall(require, "gitsigns")
 if (not status) then return end
 
 gitsigns.setup {
+  -- Default: 100,
+  update_debounce = 300,
   current_line_blame_opts = {
     delay = 1000,
   },
-  -- Default: 100,
-  update_debounce = 300,
+  signs = {
+    delete = { text = "" } -- Smaller: 
+    -- add          = { text = '│' },
+    -- change       = { text = '│' },
+    -- topdelete    = { text = '‾' },
+    -- changedelete = { text = '~' },
+    -- untracked    = { text = '┆' },
+  }
 }
