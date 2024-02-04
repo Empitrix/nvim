fb64bd45acd5d9efe9d8c20962458d850e2552be
Empitrix <empitrix@gmail.com>
2024-02-04 02:51:09 +0100

    Fixed: CMD problem between windowns & linux

--- lua/base.lua
+++ lua/base.lua
@@ -18,7 +18,7 @@ vim.opt.laststatus = 2
 -- override it for any languages in ./after/ftplugin/<lang>.vim
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
-vim.opt.shell = vim.fn.has('win32') and "pwsh" or "bash" -- cmd
+vim.opt.shell = vim.fn.has('win32') == 1 and "pwsh" or "bash" -- cmd
 vim.opt.backupskip = "/tmp/*,/private/tmp/*"
 vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
