9130e6162f9fcce132bfe685eb67a72622661d2e
empitrix <empitrix@gmail.com>
2024-10-22 06:29:49 +0330

    Changed: tsserver to ts_ls

--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -24,7 +24,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Typescript
-nvim_lsp.tsserver.setup {
+nvim_lsp.ts_ls.setup {
   capabilities = capabilities,
   settings = {
     completions = {
