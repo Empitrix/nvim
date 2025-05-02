75b11cc2a3960abb17734ea73152c62cb8fbf17e
empitrix <empitrix@gmail.com>
2025-05-02 15:05:09 +0330

    Fixed: transparent config for neovide && removed single clipboard

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,4 +1,4 @@
-vim.g.neovide_transparency = 0.95
+vim.g.neovide_opacity = 0.95
 vim.o.guifont = "FiraCode Nerd Font:h14"
 
 -- Floating Shadow
@@ -10,4 +10,3 @@ vim.g.neovide_light_radius = 5
 -- Blur
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
-
